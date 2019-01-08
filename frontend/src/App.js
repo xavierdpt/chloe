99ae9aad7e5d794a09a2c582c553213bae8eb783
xavierdpt <xavierdpt>
2019-01-08 18:42:51 +0100

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,25 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingPage from './pages/Booking';
+import MainNavigation from './components/Navigation/MainNavigation';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,11 +1,13 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './MainNavigation.css';
 
 const mainNavigation = props => (
-    <header>
+    <header className="main-navigation">
         <div className="main-navigation__logo">
             <h1>EasyEvent</h1>
         </div>
-        <nav className="main-navigation__item">
+        <nav className="main-navigation__items">
             <ul>
                 <li><NavLink to="/auth">Authenticate</NavLink></li>
                 <li><NavLink to="/events">Events</NavLink></li>
