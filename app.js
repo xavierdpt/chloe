714f8a50deb0b18877c11f3bd4acba4cf99348a5
xdptdr <xpdtdr>
2019-01-26 17:09:32 +0100

    .

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', expressGraphql({
@@ -20,6 +30,6 @@ app.use('/graphql', expressGraphql({
 
 
 mongoose.connect(`mongodb://localhost/db`,{useNewUrlParser:true}).then(()=>{
-    app.listen(3000);
+    app.listen(8000);
 }).catch(e=>console.log(e));
 
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,64 @@
 import React, { Component } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
+    state = {
+        isLogin: true
+    }   
+    constructor(props) {
+        super(props);
+        this.emailRef = React.createRef();
+        this.passwordRef = React.createRef();
+    }
+    submitHandler = event => {
+        const {isLogin} = this.state;
+        event.preventDefault();
+        
+        const email = this.emailRef.current.value;
+        const password = this.passwordRef.current.value;
+
+        if(email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+
+        const request = isLogin?{
+            query: `query { login(email: "${email}", password: "${password}") {userId token tokenExpiration}}`
+        }:{
+            query: `mutation { createUser(userInput: {email: "${email}", password: "${password}"}) { _id email }}`
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(request)
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed !');
+            }
+            return res.json();
+        }).then(console.log)
+        .catch(console.log);
+        
+    }
     render() {
-        return (<h1>Auth Page</h1>);
+        const {isLogin} = this.state;
+        return <form className="auth-form" onSubmit={this.submitHandler}>
+            <h1>{isLogin?"Log in":"Sign up"}</h1>
+            <div className="form-control">
+                <label htmlFor="email">E-Mail</label>
+                <input type="email" id="email" ref={this.emailRef} />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" ref={this.passwordRef} />
+            </div>
+            <div className="form-actions">
+            <button type="submit">Submit</button>
+            <button type="button" onClick={()=>this.setState({isLogin:!isLogin})}>Switch to {isLogin?'Signup':'Login'}</button>
+            </div>
+        </form>;
     }    
 }
 
